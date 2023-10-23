08fcabddcbc6cee93849de66dd67500f091ba595
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2023-10-23 09:00:37 +0700

    set bottomNav

--- app/src/main/java/com/damarazka/quran/MainActivity.kt
+++ app/src/main/java/com/damarazka/quran/MainActivity.kt
@@ -2,10 +2,23 @@ package com.damarazka.quran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.damarazka.quran.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavView = binding.navBottomView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
+
     }
 }
